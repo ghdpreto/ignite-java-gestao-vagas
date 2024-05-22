26a0bfc8e7930efd0bbe2f754b7da8f463ef4f7c
ghdpreto <ghdpreto@gmail.com>
2024-05-22 10:54:55 -0300

    cria primeiro repository

--- src/main/java/br/com/ghdpreto/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/ghdpreto/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,22 +1,26 @@
 package br.com.ghdpreto.gestao_vagas.modules.candidate.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.ghdpreto.gestao_vagas.modules.candidate.CandidateEntity;
+import br.com.ghdpreto.gestao_vagas.modules.candidate.CandidateRepository;
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/candidate")
 public class CandidateController {
 
-    @PostMapping("")
-    public void create(@Valid @RequestBody CandidateEntity candidateEntity) {
+    @Autowired
+    private CandidateRepository candidateRepository;
 
-        System.out.println("Candidato: ");
-        System.out.println(candidateEntity.getEmail());
+    @PostMapping("")
+    public CandidateEntity create(@Valid @RequestBody CandidateEntity candidateEntity) {
+
+        return this.candidateRepository.save(candidateEntity);
 
     }
 }

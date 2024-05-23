91822d5051cc0258c58196df840dc48dedc6fe9c
ghdpreto <ghdpreto@gmail.com>
2024-05-23 15:14:12 -0300

    feat: cadastrar job com atuh company

--- src/main/java/br/com/ghdpreto/gestao_vagas/modules/company/controllers/JobController.java
+++ src/main/java/br/com/ghdpreto/gestao_vagas/modules/company/controllers/JobController.java
@@ -1,12 +1,16 @@
 package br.com.ghdpreto.gestao_vagas.modules.company.controllers;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.ghdpreto.gestao_vagas.modules.company.dto.CreateJobDTO;
 import br.com.ghdpreto.gestao_vagas.modules.company.entities.JobEntity;
 import br.com.ghdpreto.gestao_vagas.modules.company.useCases.CreateJobUseCase;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -18,7 +22,16 @@ public class JobController {
     private CreateJobUseCase createJobUseCase;
 
     @PostMapping("")
-    public JobEntity create(@Valid @RequestBody JobEntity jobEntity) {
+    public JobEntity create(@Valid @RequestBody CreateJobDTO createJobDTO, HttpServletRequest request) {
+
+        var comapanyId = request.getAttribute("company_id");
+
+        JobEntity jobEntity = JobEntity.builder()
+                .companyId(UUID.fromString(comapanyId.toString()))
+                .benefits(createJobDTO.getBenefits())
+                .description(createJobDTO.getDescription())
+                .level(createJobDTO.getLevel())
+                .build();
 
         return this.createJobUseCase.execute(jobEntity);
     }

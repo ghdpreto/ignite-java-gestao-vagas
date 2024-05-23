1558fcc58c2b118282d1bebc922c512dd8acfd59
ghdpreto <ghdpreto@gmail.com>
2024-05-23 15:13:55 -0300

    feat: auth company

--- src/main/java/br/com/ghdpreto/gestao_vagas/security/SecurityConfig.java
+++ src/main/java/br/com/ghdpreto/gestao_vagas/security/SecurityConfig.java
@@ -1,14 +1,19 @@
 package br.com.ghdpreto.gestao_vagas.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+
+    @Autowired
+    SecurityFilter securityFilter;
 
     // informa para usar esse objeto ao inves de usar o nativo do spring
     @Bean
@@ -16,11 +21,14 @@ public class SecurityConfig {
         // desabilita a config default do security
         http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> {
+                    // rotas abertas
                     auth.requestMatchers("/candidate").permitAll()
                             .requestMatchers("/auth/company").permitAll()
                             .requestMatchers("/company").permitAll()
                             .anyRequest().authenticated();
                 })
+                // middleware
+                .addFilterBefore(securityFilter, BasicAuthenticationFilter.class)
 
         ;
 

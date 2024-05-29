86c88c1fbb647b719a713f6bc9630ffdc022d87f
ghdpreto <ghdpreto@gmail.com>
2024-05-29 14:34:40 -0300

    feat: filtro de jobs

--- src/main/java/br/com/ghdpreto/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/ghdpreto/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,5 +1,6 @@
 package br.com.ghdpreto.gestao_vagas.modules.candidate.controllers;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,10 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.ghdpreto.gestao_vagas.modules.candidate.CandidateEntity;
 import br.com.ghdpreto.gestao_vagas.modules.candidate.useCases.CreateCandidateUseCase;
+import br.com.ghdpreto.gestao_vagas.modules.candidate.useCases.ListAllJobsByFilterUseCase;
 import br.com.ghdpreto.gestao_vagas.modules.candidate.useCases.ProfileCandidateUseCase;
+import br.com.ghdpreto.gestao_vagas.modules.company.entities.JobEntity;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("candidate")
@@ -26,6 +30,9 @@ public class CandidateController {
 
     @Autowired
     private ProfileCandidateUseCase profileCandidateUseCase;
+
+    @Autowired
+    private ListAllJobsByFilterUseCase listAllJobsByFilterUseCase;
 
     @PostMapping("")
     public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidateEntity) {
@@ -57,6 +64,12 @@ public class CandidateController {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
 
+    }
+
+    @GetMapping("job")
+    @PreAuthorize("hasRole('CANDIDATE')")
+    public List<JobEntity> findJobByFilter(@RequestParam String filter) {
+        return this.listAllJobsByFilterUseCase.execute(filter);
     }
 
 }
--- src/main/java/br/com/ghdpreto/gestao_vagas/modules/company/repositories/JobRepository.java
+++ src/main/java/br/com/ghdpreto/gestao_vagas/modules/company/repositories/JobRepository.java
@@ -1,5 +1,6 @@
 package br.com.ghdpreto.gestao_vagas.modules.company.repositories;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,5 +10,9 @@ import br.com.ghdpreto.gestao_vagas.modules.company.entities.JobEntity;
 
 @Repository
 public interface JobRepository extends JpaRepository<JobEntity, UUID> {
+
+    // "contains - LIKE do sql"
+    // SELCT * FROM JOB WHERE DESCRIPTION LIKE(filter);
+    List<JobEntity> findByDescriptionContaining(String filter);
 
 }
